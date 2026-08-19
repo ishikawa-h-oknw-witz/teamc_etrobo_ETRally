23a52fc7ef231d796ce44492f78274a5739178b6
ishikawa-h-oknw-witz <ishikawa-h@oknw.witz-inc.co.jp>
2026-08-19 09:26:42 +0900

    台形制御の動作を割合に応じたものに修正

--- operation/TrapezoidCalculator.cpp
+++ operation/TrapezoidCalculator.cpp
@@ -2,9 +2,7 @@
 
 TrapezoidCalculator::TrapezoidCalculator(
             DistanceCalculator& distanceCalculator)
-    : mDistanceCalculator(distanceCalculator),
-      mAccelDistance(100.0f),
-      mDecelDistance(200.0f)
+    : mDistanceCalculator(distanceCalculator)
 {
 }
 
@@ -14,6 +12,10 @@ void TrapezoidCalculator::setParameter(TrapezoidParameter parameter)
     mMaxSpeed = parameter.maxSpeed;
     mEndSpeed = parameter.endSpeed;
     mTotalDistance = parameter.totalDistance;
+
+    // 全体距離に対する割合で加速・減速距離を決定
+    mAccelDistance = mTotalDistance * 0.2f;
+    mDecelDistance = mTotalDistance * 0.3f;
 }
 
 int TrapezoidCalculator::getSpeed()
@@ -21,15 +23,15 @@ int TrapezoidCalculator::getSpeed()
     int currentDistance = mDistanceCalculator.getDistance();
     int remainingDistance = mTotalDistance - currentDistance;
 
-    // 加速区間
+    // 加速区間：最初の20%
     if (currentDistance < mAccelDistance)
     {
         return mStartSpeed +
             (mMaxSpeed - mStartSpeed)
             * currentDistance / mAccelDistance;
     }
-    
-    // 減速区間
+
+    // 減速区間：最後の30%
     if (remainingDistance < mDecelDistance)
     {
         return mEndSpeed +
@@ -37,7 +39,6 @@ int TrapezoidCalculator::getSpeed()
             * remainingDistance / mDecelDistance;
     }
 
-    // 定速区間
+    // 定速区間：中間の50%
     return mMaxSpeed;
-
 }
