ff553315d2b2f6bfeb02f0d8ae55a5a66414559a
ishikawa-h-oknw-witz <ishikawa-h@oknw.witz-inc.co.jp>
2026-09-15 13:08:09 +0900

    色検出クラスの改良

--- detect/ColorDetector.h
+++ detect/ColorDetector.h
@@ -1,46 +1,47 @@
 #pragma once
-
+ 
 #include "ColorSensor.h"
 #include "Light.h"
-
+ 
 using namespace spikeapi;
-
+ 
 enum class Color
 {
-    None,
-    Black,
-    White,
-    Gray,
-    Green,
-    Yellow,
-    Red,
-    Blue,
-    Unknown
+    None,   // 0: 未検出
+    Black,  // 1: 黒
+    White,  // 2: 白
+    Gray,   // 3: 灰色
+    Green,  // 4: 緑
+    Yellow,// 5: 黄
+    Red,    // 6: 赤
+    Blue,   // 7: 青
+    Unknown // 8: 未知の色
 };
-
-struct ColorHSVRange
+ 
+struct ColorHSVReference
 {
     Color color;
-
-    int hMin;
-    int hMax;
-
-    int sMin;
-    int sMax;
-
-    int vMin;
-    int vMax;
+ 
+    int h;
+    int s;
+    int v;
 };
-
+ 
 class ColorDetector
 {
 public:
     ColorDetector(ColorSensor& sensor,
                   Light& light);
-
+ 
     Color detect();
-
+ 
 private:
     ColorSensor& mColorSensor;
     Light& mLight;
+ 
+    double calculateDistance(
+        const ColorSensor::HSV& hsv,
+        const ColorHSVReference& reference);
+ 
+    void setLight(Color color);
 };
